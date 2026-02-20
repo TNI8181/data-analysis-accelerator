180d82f85edf8d6ea50f419ad3bb15da4241a009
TNI8181 <tni8181@gmail.com>
2026-02-20 12:59:27 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,3 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Data Analysis Accelerator",
-    layout="wide"
-)
-
-st.title("Data Analysis Accelerator")
-st.markdown("Select a module from the left navigation.")
+st.switch_page("pages/Upload & Profile.py")
